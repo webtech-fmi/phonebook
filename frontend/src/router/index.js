06719d696c4d80e69f79347415360a17439a2a6c
Velko Bonev <velkob@uni-sofia.bg>
2021-03-29 16:56:09 +0300

    frontend: Added AddContact.vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../views/Login.vue';
 import Home from '../views/Home.vue';
+import AddContact from '../views/AddContact.vue';
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login,
+  },
+  {
+    path: '/add-contact',
+    name: 'AddContact',
+    component: AddContact,
   },
 ];
 
