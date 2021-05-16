77b0edebdf5cb85b85ec188929611aa24493c554
merilin <m.pisina@gaida.ai>
2021-05-16 18:10:31 +0300

    phonebook-api: Remove config

--- backend/phonebook-api/routes/index.js
+++ backend/phonebook-api/routes/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const users = require("./users");
-// const port = 3000
 
 router.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
