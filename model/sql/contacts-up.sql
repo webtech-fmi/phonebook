22096e4c15ed33fe373aef78d9afb8c62e4a13f8
merilin <m.pisina@gaida.ai>
2021-06-13 11:11:48 +0300

    model: Update queries

--- model/sql/contacts-up.sql
+++ model/sql/contacts-up.sql
@@ -1,13 +1,12 @@
-CREATE TABLE IF NOT EXISTS contacts
-(
-	id UUID
-		CONSTRAINT pk_contacts PRIMARY KEY,
-	owner_id UUID NOT NULL,
-	created_time TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+CREATE TABLE IF NOT EXISTS contacts (
+    id UUID CONSTRAINT pk_contacts PRIMARY KEY,
+    owner_id UUID NOT NULL,
+    created_time TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
     modified_time TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+    status TEXT NOT NULL CONSTRAINT status_in CHECK (status IN ('active', 'archived')),
     favourite BOOLEAN DEFAULT FALSE,
     email JSONB,
     personal JSONB,
     phone JSONB,
-   	metadata JSONB
+    metadata JSONB
 );
--- model/sql/profiles-up.sql
+++ model/sql/profiles-up.sql
@@ -1,14 +1,11 @@
-CREATE TABLE IF NOT EXISTS profiles
-(
-	id UUID
-		CONSTRAINT pk_profiles PRIMARY KEY
-		CONSTRAINT fk_profiles_contacts REFERENCES contacts (owner_id),
-	user_id UUID
-		CONSTRAINT fk_profiles_users REFERENCES users (id),
+CREATE TABLE IF NOT EXISTS profiles (
+	id UUID CONSTRAINT pk_profiles PRIMARY KEY CONSTRAINT fk_profiles_contacts REFERENCES contacts (owner_id),
+	user_id UUID CONSTRAINT fk_profiles_users REFERENCES users (id),
 	created_time TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
-    modified_time TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
-    email JSONB,
-    personal JSONB,
-    phone JSONB,
-   	metadata JSONB
+	modified_time TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+	consent BOOLEAN DEFAULT FALSE,
+	email JSONB,
+	personal JSONB,
+	phone JSONB,
+	metadata JSONB
 );
