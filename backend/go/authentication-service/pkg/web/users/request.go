8146d2076533d38dd661a2b1c5c3f5d58a103f6a
merilin <m.pisina@gaida.ai>
2021-06-13 04:28:38 +0300

    storage: Add redis to docker-compose.

--- backend/go/authentication-service/pkg/web/users/request.go
+++ backend/go/authentication-service/pkg/web/users/request.go
@@ -17,7 +17,7 @@ func (cr *CreateRequest) Validate() error {
 	return ozzo.ValidateStruct(
 		cr,
 		ozzo.Field(&cr.FullName, ozzo.Required, ozzo.Length(5, 25)),
-		ozzo.Field(&cr.Email, ozzo.Required, ozzo.Length(8, 50), is.Email),
+		ozzo.Field(&cr.Email, ozzo.Required, is.Email),
 		ozzo.Field(&cr.Password, ozzo.Required, ozzo.Length(8, 32)),
 	)
 }
